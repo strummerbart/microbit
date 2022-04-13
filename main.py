2520d59dbcf47625ad3297c46c024bd307185e5d
strummerbart <103561619+strummerbart@users.noreply.github.com>
2022-04-13 14:29:06 +0200

    Show a X on screen

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
-def display_hello_world():
-    basic.show_string("Hello World!")
-    
+def plot_draw():
+    led.plot_brightness(0, 0, new_brigth)
+    led.plot_brightness(4, 0, new_brigth)
+    led.plot_brightness(1, 1, new_brigth)
+    led.plot_brightness(3, 1, new_brigth)
+    led.plot_brightness(2, 2, new_brigth)
+    led.plot_brightness(1, 3, new_brigth)
+    led.plot_brightness(0, 4, new_brigth)
+    led.plot_brightness(3, 3, new_brigth)
+    led.plot_brightness(4, 4, new_brigth)
+
+
+
+RATE = 2
+MAX_BRIGTH = 255
+MIN_BRIGTH = 0
+action = "decrease"
+new_brigth = MAX_BRIGTH
+
+#ON START
+plot_draw()
+basic.pause(2000)
+
+def on_forever():
+    global action
+    global new_brigth
+    led_brigthness = led.point_brightness(2, 2)
+
+    if action == "decrease":
+        new_brigth = led_brigthness - RATE
+        if new_brigth <= MIN_BRIGTH:
+            new_brigth = MIN_BRIGTH
+            action = "increase"
+    elif action == "increase":
+        new_brigth = led_brigthness + RATE
+        if new_brigth >= MAX_BRIGTH:
+            new_brigth = MAX_BRIGTH
+            action = "decrease"
+        
+    plot_draw()
+
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,42 @@
-function display_hello_world() {
-    basic.showString("Hello World!")
+function plot_draw() {
+    led.plotBrightness(0, 0, new_brigth)
+    led.plotBrightness(4, 0, new_brigth)
+    led.plotBrightness(1, 1, new_brigth)
+    led.plotBrightness(3, 1, new_brigth)
+    led.plotBrightness(2, 2, new_brigth)
+    led.plotBrightness(1, 3, new_brigth)
+    led.plotBrightness(0, 4, new_brigth)
+    led.plotBrightness(3, 3, new_brigth)
+    led.plotBrightness(4, 4, new_brigth)
 }
 
+let RATE = 2
+let MAX_BRIGTH = 255
+let MIN_BRIGTH = 0
+let action = "decrease"
+let new_brigth = MAX_BRIGTH
+// ON START
+plot_draw()
+basic.pause(2000)
+basic.forever(function on_forever() {
+    
+    
+    let led_brigthness = led.pointBrightness(2, 2)
+    if (action == "decrease") {
+        new_brigth = led_brigthness - RATE
+        if (new_brigth <= MIN_BRIGTH) {
+            new_brigth = MIN_BRIGTH
+            action = "increase"
+        }
+        
+    } else if (action == "increase") {
+        new_brigth = led_brigthness + RATE
+        if (new_brigth >= MAX_BRIGTH) {
+            new_brigth = MAX_BRIGTH
+            action = "decrease"
+        }
+        
+    }
+    
+    plot_draw()
+})
