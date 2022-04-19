6db49c0a76114610b08a3c0622257343dd897d63
strummerbart <103561619+strummerbart@users.noreply.github.com>
2022-04-19 17:47:32 +0200

    test sulla seriale

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-def display_hello_world():
-    basic.show_string("Hello World!")
+
+def on_forever():            
+    light_level = input.light_level()
+    if (light_level == 0):
+        serial.write_line("Buio")
+    else:
+        serial.write_line("Luce")
+
+basic.forever(on_forever)
     
--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-function display_hello_world() {
-    basic.showString("Hello World!")
-}
-
+basic.forever(function on_forever() {
+    let light_level = input.lightLevel()
+    if (light_level == 0) {
+        serial.writeLine("Buio")
+    } else {
+        serial.writeLine("Luce")
+    }
+    
+})
