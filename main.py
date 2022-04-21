322a00c93b85372863873ae207ca08c45d52a250
strummerbart <103561619+strummerbart@users.noreply.github.com>
2022-04-21 17:37:50 +0200

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 def on_forever():            
     light_level = input.light_level()
     if (light_level == 0):
+        basic.show_number(0)
         serial.write_line("Buio")
     else:
+        basic.show_number(light_level)
         serial.write_line("Luce")
 
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,9 +1,32 @@
+function plot_draw() {
+    led.plotBrightness(0, 0, new_brigth)
+    led.plotBrightness(4, 0, new_brigth)
+    led.plotBrightness(1, 1, new_brigth)
+    led.plotBrightness(3, 1, new_brigth)
+    led.plotBrightness(2, 2, new_brigth)
+    led.plotBrightness(1, 3, new_brigth)
+    led.plotBrightness(0, 4, new_brigth)
+    led.plotBrightness(3, 3, new_brigth)
+    led.plotBrightness(4, 4, new_brigth)
+}
+
+let RATE = 2
+let MAX_BRIGTH = 255
+let MIN_BRIGTH = 0
+let action = "decrease"
+let new_brigth = MAX_BRIGTH
+// ON START
+plot_draw()
+basic.pause(2000)
 basic.forever(function on_forever() {
     let light_level = input.lightLevel()
     if (light_level == 0) {
+        basic.showNumber(0)
         serial.writeLine("Buio")
     } else {
+        basic.showNumber(light_level)
         serial.writeLine("Luce")
     }
     
+    plot_draw()
 })
