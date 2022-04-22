8a43cf9160f421bd91836b0464c53498a4fc8884
strummerbart <103561619+strummerbart@users.noreply.github.com>
2022-04-22 12:05:33 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,33 +1,78 @@
-SPEED = 500
-level = 5
+def fall_a_drop(x_coordinate: number):
+    
+    level = levels[x_coordinate]
 
-def switch_led_off(x, y):
+    if level == 1 and led.point(x_coordinate, 0) == False:
+        led.plot(x_coordinate, 0)
+        print("Plot last led on level 1. x:" + str(x_coordinate))
+        return
+
+    SPEED = randint(50, 150)    
+
+    # disegna il primo led su prima riga
+    led.plot(x_coordinate, 0)
+    basic.pause(SPEED)
+    switch_led_off(x_coordinate, 0)
+
+    for y_coordinate in range(1, level):
+        led.plot(x_coordinate, y_coordinate)
+        basic.pause(SPEED)
+
+        #evita di spegnere l'ultimo led della colonna
+        if y_coordinate < level - 1:
+            switch_led_off(x_coordinate, y_coordinate)
+
+    basic.pause(SPEED)
+    new_level = level - 1
+    levels[x_coordinate] = new_level
+
+def switch_led_off(x: number, y: number):    
     bright = led.point_brightness(x, y)
+
     while bright > 0:
-        led.unplot(x, y)
-        bright -= 10
+        led.unplot(x, y)                
+        
+        if bright > 0:
+            bright = (bright - 10)
+        else: bright = 0
+        
         led.plot_brightness(x, y, bright)
         pause(10)
 
-def fall_a_drop(x_coordinate: number):
-    global SPEED
-    global level
-    SPEED = randint(50, 150)
-    led.plot(x_coordinate, 0)
-    basic.pause(SPEED)
-    switch_led_off(x_coordinate, 0)    
-    for y_coordinate in range(1, 5):
-        led.plot(x_coordinate, y_coordinate)
-        basic.pause(SPEED)                
-        switch_led_off(x_coordinate, y_coordinate)
+SPEED = 500
+levels = [5, 5, 5, 5, 5]
 
-    basic.pause(SPEED)
+def restart_screen():
+    global levels
+
+    pause(1000)
+    basic.clear_screen()
+    levels = [5, 5, 5, 5, 5]
+    print("----------------------------")
+
+def screen_full():
+    global levels
+
+    print(levels)
+    for y in range(5):
+        for x in range(5):
+            if (levels[x] > 1):
+                return False
+    
+    for x in range(5):
+        if (led.point(x, 0) == False):
+            return False
+            
+    return True                
 
 def on_forever():
-    for index in range(4):
-        x1 = randint(0, 4)        
-        if led.point(x1, 0) == False:
-            fall_a_drop(x1)
-
+   
+    if screen_full():
+        restart_screen()
+    else:
+        for index in range(6):
+            x = randint(0, 4)
+            if led.point(x, 0) == False:
+                fall_a_drop(x)
 
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,37 +1,88 @@
-let SPEED = 500
-let level = 5
+function fall_a_drop(x_coordinate: number) {
+    let level = levels[x_coordinate]
+    if (level == 1 && led.point(x_coordinate, 0) == false) {
+        led.plot(x_coordinate, 0)
+        console.log("Plot last led on level 1. x:" + ("" + x_coordinate))
+        return
+    }
+    
+    let SPEED = randint(50, 150)
+    //  disegna il primo led su prima riga
+    led.plot(x_coordinate, 0)
+    basic.pause(SPEED)
+    switch_led_off(x_coordinate, 0)
+    for (let y_coordinate = 1; y_coordinate < level; y_coordinate++) {
+        led.plot(x_coordinate, y_coordinate)
+        basic.pause(SPEED)
+        // evita di spegnere l'ultimo led della colonna
+        if (y_coordinate < level - 1) {
+            switch_led_off(x_coordinate, y_coordinate)
+        }
+        
+    }
+    basic.pause(SPEED)
+    let new_level = level - 1
+    levels[x_coordinate] = new_level
+}
+
 function switch_led_off(x: number, y: number) {
     let bright = led.pointBrightness(x, y)
     while (bright > 0) {
         led.unplot(x, y)
-        bright -= 10
+        if (bright > 0) {
+            bright = bright - 10
+        } else {
+            bright = 0
+        }
+        
         led.plotBrightness(x, y, bright)
         pause(10)
     }
 }
 
-function fall_a_drop(x_coordinate: number) {
+let SPEED = 500
+let levels = [5, 5, 5, 5, 5]
+function restart_screen() {
     
-    
-    SPEED = randint(50, 150)
-    led.plot(x_coordinate, 0)
-    basic.pause(SPEED)
-    switch_led_off(x_coordinate, 0)
-    for (let y_coordinate = 1; y_coordinate < 5; y_coordinate++) {
-        led.plot(x_coordinate, y_coordinate)
-        basic.pause(SPEED)
-        switch_led_off(x_coordinate, y_coordinate)
-    }
-    basic.pause(SPEED)
+    pause(1000)
+    basic.clearScreen()
+    levels = [5, 5, 5, 5, 5]
+    console.log("----------------------------")
 }
 
-basic.forever(function on_forever() {
-    let x1: number;
-    for (let index = 0; index < 4; index++) {
-        x1 = randint(0, 4)
-        if (led.point(x1, 0) == false) {
-            fall_a_drop(x1)
+function screen_full(): boolean {
+    let x: number;
+    
+    console.log(levels)
+    for (let y = 0; y < 5; y++) {
+        for (x = 0; x < 5; x++) {
+            if (levels[x] > 1) {
+                return false
+            }
+            
+        }
+    }
+    for (x = 0; x < 5; x++) {
+        if (led.point(x, 0) == false) {
+            return false
         }
         
     }
+    return true
+}
+
+basic.forever(function on_forever() {
+    let x: number;
+    if (screen_full()) {
+        restart_screen()
+    } else {
+        for (let index = 0; index < 6; index++) {
+            x = randint(0, 4)
+            if (led.point(x, 0) == false) {
+                fall_a_drop(x)
+            }
+            
+        }
+    }
+    
 })
